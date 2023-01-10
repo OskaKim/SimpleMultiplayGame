46f0d28051d2d98164711762a3d93304516bac84
OskaKim <o2kim0225@naver.com>
2023-01-10 23:10:05 +0900

    lobby login sample

---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Lobbies.Models;
+using UnityEngine;
+
+public class LobbyManager : MonoBehaviour {
+    private void Start() {
+        UnityServices.InitializeAsync();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.F)) {
+            LobbyUser lobbyUser = new LobbyUser();
+            lobbyUser.DisplayName = "hoge";
+            CreateLobbyAsync("lobby_name", 2, false, lobbyUser, (Lobby lobby) => {
+                Debug.Log("success");
+            }, () => {
+                Debug.Log("failed");
+            });
+        }
+    }
+    public void CreateLobbyAsync(string lobbyName, int maxPlayers, bool isPrivate, LobbyUser localUser, Action<Lobby> onSuccess, Action onFailure) {
+        //if (!m_rateLimitHost.CanCall()) {
+        //    onFailure?.Invoke();
+        //    UnityEngine.Debug.LogWarning("Create Lobby hit the rate limit.");
+        //    return;
+        //}
+
+        string uasId = AuthenticationService.Instance.PlayerId;
+        LobbyAPIInterface.CreateLobbyAsync(uasId, lobbyName, maxPlayers, isPrivate, CreateInitialPlayerData(localUser), OnLobbyCreated);
+
+        void OnLobbyCreated(Lobby response) {
+            if (response == null)
+                onFailure?.Invoke();
+            else
+                onSuccess?.Invoke(response); // The Create request automatically joins the lobby, so we need not take further action.
+        }
+    }
+    private static Dictionary<string, PlayerDataObject> CreateInitialPlayerData(LobbyUser player) {
+        Dictionary<string, PlayerDataObject> data = new Dictionary<string, PlayerDataObject>();
+        PlayerDataObject dataObjName = new PlayerDataObject(PlayerDataObject.VisibilityOptions.Member, player.DisplayName);
+        data.Add("DisplayName", dataObjName);
+        return data;
+    }
+
+}
